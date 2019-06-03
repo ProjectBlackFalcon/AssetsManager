fe77d01564597549a7b50d2d87b68aeecc01833d
Alexis VIALARET <alexisvlrt@gmail.com>
2019-06-03 10:56:36 +0200

    Some hashes were different for the same content.

--- assets_uploader.py
+++ assets_uploader.py
@@ -1,13 +1,13 @@
 import json
 import pymongo
-import mongo
+from credentials import credentials
 import os
 import hashlib
 
 
 def generate_file_md5(path, blocksize=2**20):
     m = hashlib.md5()
-    with open(path, "rb" ) as f:
+    with open(path, "rb") as f:
         while True:
             buf = f.read(blocksize)
             if not buf:
@@ -16,7 +16,7 @@ def generate_file_md5(path, blocksize=2**20):
     return m.hexdigest()
 
 
-client = pymongo.MongoClient(host=mongo.host, port=mongo.port, username=mongo.username, password=mongo.password)
+client = pymongo.MongoClient(host=credentials['mongo']['host'], port=credentials['mongo']['port'], username=credentials['mongo']['username'], password=credentials['mongo']['password'])
 assets_path = ['raw_transformer/definitive_output/' + file for file in os.listdir('raw_transformer/definitive_output/')] + \
               ['raw_transformer/static_data/' + file for file in os.listdir('raw_transformer/static_data/')]
 
@@ -27,15 +27,16 @@ for file_path in assets_path:
         with open(file_path, 'r', encoding='utf8') as f:
             data = json.load(f)
 
-
         checksum = generate_file_md5(file_path)
-        existing_version = client.blackfalcon.files.find_one({"filename": file.replace('.json', '')})
+        existing_version = client.blackfalcon.checksums.find_one({"filename": file.replace('.json', '')})
         if existing_version is not None:
             if existing_version['checksum_md5'] == checksum:
                 print('{} is up to date'.format(file))
             else:
                 print('Updating {}'.format(file))
-                client.blackfalcon.files.find_one_and_replace({"filename": file.replace('.json', '')}, {'filename': file.replace('.json', ''), 'payload': data, 'checksum_md5': checksum})
+                client.blackfalcon.files.find_one_and_replace({"filename": file.replace('.json', '')}, {'filename': file.replace('.json', ''), 'payload': data})
+                client.blackfalcon.checksums.find_one_and_replace({"filename": file.replace('.json', '')}, {'filename': file.replace('.json', ''), 'checksum_md5': checksum})
         else:
             print('Creating {}'.format(file))
-            client.blackfalcon.files.insert_one({'filename': file.replace('.json', ''), 'payload': data, 'checksum_md5': checksum})
+            client.blackfalcon.files.insert_one({'filename': file.replace('.json', ''), 'payload': data})
+            client.blackfalcon.checksums.insert_one({'filename': file.replace('.json', ''), 'checksum_md5': checksum})
