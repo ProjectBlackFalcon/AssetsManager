9298edec0195d39bfb158b49944db8b5099bf75d
avialaret <alexisvlrt@gmail.com>
2019-06-07 16:44:16 +0200

    Supports being launched from command line

--- assets_uploader.py
+++ assets_uploader.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 import pymongo
 from credentials import credentials
 import os
@@ -15,15 +17,13 @@ def generate_file_md5(path, blocksize=2**20):
             m.update(buf)
     return m.hexdigest()
 
-
 client = pymongo.MongoClient(host=credentials['mongo']['host'], port=credentials['mongo']['port'], username=credentials['mongo']['username'], password=credentials['mongo']['password'])
-assets_path = ['raw_transformer/definitive_output/' + file for file in os.listdir('raw_transformer/definitive_output/')] + \
-              ['raw_transformer/static_data/' + file for file in os.listdir('raw_transformer/static_data/')]
+assets_path = [os.path.abspath(os.path.join(os.path.dirname(__file__), 'raw_transformer/definitive_output/', file)) for file in os.listdir(os.path.abspath(os.path.join(os.path.dirname(__file__), 'raw_transformer/definitive_output/')))] + \
+              [os.path.abspath(os.path.join(os.path.dirname(__file__), 'raw_transformer/static_data/', file)) for file in os.listdir(os.path.abspath(os.path.join(os.path.dirname(__file__), 'raw_transformer/static_data/')))]
 
 for file_path in assets_path:
     if file_path.endswith('.json'):
-        file = file_path.split('/')[-1]
-        print(file)
+        file = os.path.basename(file_path)
         with open(file_path, 'r', encoding='utf8') as f:
             data = json.load(f)
 
@@ -36,15 +36,15 @@ for file_path in assets_path:
                 print('Updating {}'.format(file))
                 client.blackfalcon.files.find_one_and_replace({"filename": file.replace('.json', '')}, {'filename': file.replace('.json', ''), 'payload': data})
                 stored_file = client.blackfalcon.files.find_one({"filename": file.replace('.json', '')})
-                with open('tmp/_', 'w', encoding='utf-8') as f:
+                with open('_', 'w', encoding='utf-8') as f:
                     json.dump(stored_file['payload'], f, ensure_ascii=False)
-                checksum = generate_file_md5('tmp/_')
+                checksum = generate_file_md5('_')
                 client.blackfalcon.checksums.find_one_and_replace({"filename": file.replace('.json', '')}, {'filename': file.replace('.json', ''), 'checksum_md5': checksum})
         else:
             print('Creating {}'.format(file))
             client.blackfalcon.files.insert_one({'filename': file.replace('.json', ''), 'payload': data})
             stored_file = client.blackfalcon.files.find_one({"filename": file.replace('.json', '')})
-            with open('tmp/_', 'w', encoding='utf-8') as f:
-                f.write(stored_file)
-            checksum = generate_file_md5('tmp/_')
+            with open('_', 'w', encoding='utf-8') as f:
+                json.dump(stored_file['payload'], f, ensure_ascii=False)
+            checksum = generate_file_md5('_')
             client.blackfalcon.checksums.insert_one({'filename': file.replace('.json', ''), 'checksum_md5': checksum})
