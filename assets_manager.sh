7bfe24c87fc303458bbb5f13568f906140685cad
Ugdha <alexisvlrt@gmail.com>
2019-09-27 15:50:00 +0200

    sudo compatible

--- assets_manager.sh
+++ assets_manager.sh
@@ -3,6 +3,6 @@
 cd /home/docker/AssetsManager
 git reset --hard HEAD
 git pull
-python3.7 raw_transformer/assets_transformer.py ~/.config/Ankama/zaap/dofus
+python3.7 raw_transformer/assets_transformer.py /home/docker/.config/Ankama/zaap/dofus
 python3.7 assets_uploader.py
 cd /home/docker
